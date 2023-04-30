f091d7e0642b1f8aa1135277c2ca8749a9478f5f
Washington Antunes <washington.cead@gmail.com>
2023-04-30 17:56:02 -0300

    Formulário Reativo
    Foi importado no home.module.ts o modulo ReactiveFormsModule
    Foi adicionado declarado um parâmetro que será o FormGroup e iniciando com o formBuilder
    No arquivo html o formulário foi configurado o [formGroup] apontando para o parâmetro declarado e o submit para o método cadastrar
    E cada input foi adicionado o formControlName

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NovoUsuarioService } from './novo-usuario.service';
+import { NovoUsuario } from './novo-usuario';
 
 @Component({
   selector: 'app-novo-usuario',
   templateUrl: './novo-usuario.component.html',
-  styleUrls: ['./novo-usuario.component.css']
+  styleUrls: ['./novo-usuario.component.css'],
 })
-export class NovoUsuarioComponent {
+export class NovoUsuarioComponent implements OnInit {
+  novoUsuarioForm!: FormGroup;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private novoUsuarioService: NovoUsuarioService
+  ) {}
+
+  ngOnInit(): void {
+    this.novoUsuarioForm = this.formBuilder.group({
+      email: [''],
+      fullName: [''],
+      userName: [''],
+      password: [''],
+    });
+  }
+
+  cadastrar() {
+    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+    console.log(novoUsuario);
+  }
 }
