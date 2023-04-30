ed1ff3680785a219872dd286e902c50bfa7ff4f2
Washington Antunes <washington.cead@gmail.com>
2023-04-30 17:25:29 -0300

    Criando Componente
    Foi criado o componente novo-usuario usando o comando: ng g c home/novo-usuario
    Foi adicionado a rota do novo-usuario no arquivo home-routing.module.ts

---
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-novo-usuario',
+  templateUrl: './novo-usuario.component.html',
+  styleUrls: ['./novo-usuario.component.css']
+})
+export class NovoUsuarioComponent {
+
+}
