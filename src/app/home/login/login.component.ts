a26888da12f99f9de20aa44de6c165b22a0792f5
Washington Antunes <washington.cead@gmail.com>
2023-04-30 16:18:57 -0300

    Formulário Template-Driven

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  usuario = '';
+  senha = '';
 
+  login() {
+    console.log(this.usuario);
+    console.log(this.senha);
+  }
 }
