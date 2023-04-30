c205ee99ba517944d7f179df07ef9a824e3de82a
Washington Antunes <washington.cead@gmail.com>
2023-04-30 16:54:05 -0300

    Redirecionando a rota para lista de animais em caso de sucesso no login
    Foi criado o module animais com o comando: ng g m animais --routing
    Foi criado o componente lista-animais com  o comando: ng g c animais/lista-animais
    Foi adicionado o mapeamento lazy load no app-routing.module.ts
    Foi configurado o redirecionamento no login.component.ts

---
+++ src/app/animais/lista-animais/lista-animais.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-animais',
+  templateUrl: './lista-animais.component.html',
+  styleUrls: ['./lista-animais.component.css']
+})
+export class ListaAnimaisComponent {
+
+}
--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
 
 @Component({
@@ -10,12 +11,15 @@ export class LoginComponent {
   usuario = '';
   senha = '';
 
-  constructor(private authService: AutenticacaoService) {}
+  constructor(
+    private authService: AutenticacaoService,
+    private router: Router
+  ) {}
 
   login() {
     this.authService.autenticar(this.usuario, this.senha).subscribe(
       () => {
-        console.log('Autenticado com sucesso');
+        this.router.navigate(['animais']);
       },
       (error) => {
         alert('Usuario ou senha inválido');
