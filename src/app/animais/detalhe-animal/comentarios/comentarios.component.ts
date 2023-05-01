788b9b12c194d41126bebcebcc25bb3871efe4e8
Washington Antunes <washington.cead@gmail.com>
2023-05-01 20:38:53 -0300

    Comentários - Template
    Foi importado no animais.module.ts o MensagemModule e ReactiveFormsModule
    Foi incluído as tag no comentarios.component.html
    Foi criado os dois métodos no comentarios.component.ts

--- src/app/animais/detalhe-animal/comentarios/comentarios.component.ts
+++ src/app/animais/detalhe-animal/comentarios/comentarios.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Comentarios } from './comentarios';
+import { Observable, switchMap, tap } from 'rxjs';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ComentariosService } from './comentarios.service';
 
 @Component({
   selector: 'app-comentarios',
   templateUrl: './comentarios.component.html',
-  styleUrls: ['./comentarios.component.css']
+  styleUrls: ['./comentarios.component.css'],
 })
-export class ComentariosComponent {
+export class ComentariosComponent implements OnInit {
+  @Input() id!: number;
+  comentarios$!: Observable<Comentarios>;
+  comentarioForm!: FormGroup;
 
+  constructor(
+    private comentariosService: ComentariosService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.comentarios$ = this.comentariosService.buscaComentario(this.id);
+    this.comentarioForm = this.formBuilder.group({
+      comentario: ['', Validators.maxLength(300)],
+    });
+  }
+
+  gravar(): void {
+    const comentario = this.comentarioForm.get('comentario')?.value ?? '';
+    this.comentarios$ = this.comentariosService
+      .incluirComentario(this.id, comentario)
+      .pipe(
+        switchMap(() => this.comentariosService.buscaComentario(this.id)),
+        tap(() => {
+          this.comentarioForm.reset();
+          alert('Salvo Comentario');
+        })
+      );
+  }
 }
