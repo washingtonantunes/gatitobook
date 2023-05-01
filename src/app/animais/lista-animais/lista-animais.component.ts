e1d3f3891a8fff987689aa15c5114408fc55ac92
Washington Antunes <washington.cead@gmail.com>
2023-04-30 23:53:38 -0300

    Lista de animais
    Adicionado o app-grade-fotos-animais no lista-animais.component.html
    Iniciando o parâmetro animais no lista-animais.component.ts

--- src/app/animais/lista-animais/lista-animais.component.ts
+++ src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Animais } from '../animais';
+import { UsuarioService } from 'src/app/autenticacao/usuario/usuario.service';
+import { AnimaisService } from '../animais.service';
 
 @Component({
   selector: 'app-lista-animais',
   templateUrl: './lista-animais.component.html',
-  styleUrls: ['./lista-animais.component.css']
+  styleUrls: ['./lista-animais.component.css'],
 })
-export class ListaAnimaisComponent {
+export class ListaAnimaisComponent implements OnInit {
+  animais!: Animais;
 
+  constructor(
+    private usuarioService: UsuarioService,
+    private animaisService: AnimaisService
+  ) {}
+
+  ngOnInit(): void {
+    this.usuarioService.retornaUsuario().subscribe((usuario) => {
+      const userName = usuario.name ?? '';
+      this.animaisService.listaDoUsuario(userName).subscribe((animais) => {
+        this.animais = animais;
+      });
+    });
+  }
 }
