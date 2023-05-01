3f4298c8346bd3b00d90918ac712b38ccfc04c82
Washington Antunes <washington.cead@gmail.com>
2023-05-01 18:06:37 -0300

    Curtir e excluir foto
    Foi criado os métodos no arquivo animais.service.ts
    Foi adicionado os componentes no arquivo html
    Foi criado os métodos no arquivo ts

--- src/app/animais/animais.service.ts
+++ src/app/animais/animais.service.ts
@@ -1,10 +1,11 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Animais, Animal } from './animais';
-import { Observable } from 'rxjs';
+import { Observable, catchError, mapTo, of, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 const API = environment.apiURL;
+const NOT_MODIFIED = 304;
 
 @Injectable({
   providedIn: 'root',
@@ -18,5 +19,20 @@ export class AnimaisService {
 
   buscaPorId(id: number): Observable<Animal> {
     return this.httpClient.get<Animal>(`${API}/photos/${id}`);
+  }
+
+  excluiAnimal(id: number): Observable<Animal> {
+    return this.httpClient.delete<Animal>(`${API}/photos/${id}`);
+  }
+
+  curtir(id: number): Observable<boolean> {
+    return this.httpClient
+      .post(`${API}/photos/${id}/like`, {}, { observe: 'response' })
+      .pipe(
+        mapTo(true),
+        catchError((error) => {
+          return error.status === NOT_MODIFIED ? of(false) : throwError(error);
+        })
+      );
   }
 }
