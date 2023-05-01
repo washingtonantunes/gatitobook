7da7d7a8d27b5792f273b88dc3b98309f2636999
Washington Antunes <washington.cead@gmail.com>
2023-04-30 21:41:19 -0300

    Criação do componente Animal
    Criado o arquivo html
    Inserido duas variaveis que serão recebidas no componente

---
+++ src/app/animais/animal/animal.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input } from '@angular/core';
+
+const API = 'http://localhost:3000';
+
+@Component({
+  selector: 'app-animal',
+  templateUrl: './animal.component.html',
+  styleUrls: ['./animal.component.css'],
+})
+export class AnimalComponent {
+  private urlOriginal = '';
+
+  @Input() descricao = '';
+
+  @Input() set url(url: string) {
+    if (url.startsWith('data')) {
+      this.urlOriginal = url;
+    } else {
+      this.urlOriginal = `${API}/imgs/${url}`;
+    }
+  }
+
+  get url(): string {
+    return this.urlOriginal;
+  }
+}
