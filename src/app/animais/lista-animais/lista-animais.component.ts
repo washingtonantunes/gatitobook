f5c71941a831839d5d6d36e4c8ae33ff8cd9e081
Washington Antunes <washington.cead@gmail.com>
2023-05-01 00:06:01 -0300

    Utilizado RXJS
    Alterando o código para remover o uso de subscribe hell

--- src/app/animais/lista-animais/lista-animais.component.ts
+++ src/app/animais/lista-animais/lista-animais.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Animais } from '../animais';
 import { UsuarioService } from 'src/app/autenticacao/usuario/usuario.service';
 import { AnimaisService } from '../animais.service';
+import { Observable, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-lista-animais',
@@ -9,7 +10,7 @@ import { AnimaisService } from '../animais.service';
   styleUrls: ['./lista-animais.component.css'],
 })
 export class ListaAnimaisComponent implements OnInit {
-  animais!: Animais;
+  animais$!: Observable<Animais>;
 
   constructor(
     private usuarioService: UsuarioService,
@@ -17,11 +18,11 @@ export class ListaAnimaisComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.usuarioService.retornaUsuario().subscribe((usuario) => {
-      const userName = usuario.name ?? '';
-      this.animaisService.listaDoUsuario(userName).subscribe((animais) => {
-        this.animais = animais;
-      });
-    });
+    this.animais$ = this.usuarioService.retornaUsuario().pipe(
+      switchMap((usuario) => {
+        const userName = usuario.name ?? '';
+        return this.animaisService.listaDoUsuario(userName);
+      })
+    );
   }
 }
