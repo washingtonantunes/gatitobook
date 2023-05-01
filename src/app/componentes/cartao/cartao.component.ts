22803a3c41a8d96af937c46bd09429ad993abfc9
Washington Antunes <washington.cead@gmail.com>
2023-04-30 22:06:10 -0300

    Criação do componente Cartao
    Foi criado o modulo cartão e o componente cartão
    Foi exportado para quem precisar utilizar no cartao.module.ts
    Foi usado o ng-content para projetar conteúdo dentro do componente

---
+++ src/app/componentes/cartao/cartao.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-cartao',
+  templateUrl: './cartao.component.html',
+  styleUrls: ['./cartao.component.css'],
+})
+export class CartaoComponent {
+  @Input() titulo = '';
+}
