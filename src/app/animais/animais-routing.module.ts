f7e2ef728a39b689baaf00d4e57dd1563dca1fd6
Washington Antunes <washington.cead@gmail.com>
2023-05-01 16:19:39 -0300

    Componente de detalhe
    Foi criado o componente detalhe-animal com o comando: ng g c animais/detalhe-animal
    Foi adicionado a rota no arquivo animais-routing.module.ts
    Foi corrigido o arquivo grade-fotos-animais.component.html para tornar o app-cartao clicavel
    Foi criado o método buscaPorId no animais.service.ts

--- src/app/animais/animais-routing.module.ts
+++ src/app/animais/animais-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
+import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ListaAnimaisComponent,
+  },
+  {
+    path: ':animalId',
+    component: DetalheAnimalComponent,
   },
 ];
 
--- src/app/animais/animais.service.ts
+++ src/app/animais/animais.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Animais } from './animais';
+import { Animais, Animal } from './animais';
 import { Observable } from 'rxjs';
 import { TokenService } from '../autenticacao/token.service';
 import { environment } from 'src/environments/environment';
@@ -22,5 +22,11 @@ export class AnimaisService {
     return this.httpClient.get<Animais>(`${API}/${nomeDoUsuario}/photos`, {
       headers,
     });
+  }
+
+  buscaPorId(id: number): Observable<Animal> {
+    const token = this.tokenService.retornaToken();
+    const headers = new HttpHeaders().append('x-access-token', token);
+    return this.httpClient.get<Animal>(`${API}/photos/${id}`, { headers });
   }
 }
