8fb4957e7191daed7b206a391a318288a13ca9b4
Washington Antunes <washington.cead@gmail.com>
2023-04-30 23:25:03 -0300

    Grade de fotos dos animais
    Foi importado o CartaoModule no arquivo: animais.module.ts
    Foi criado o arquivo html usando os componentes app-cartao e app-anmal

---
+++ src/app/animais/grade-fotos-animais/grade-fotos-animais.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { Animais } from '../animais';
+
+@Component({
+  selector: 'app-grade-fotos-animais',
+  templateUrl: './grade-fotos-animais.component.html',
+  styleUrls: ['./grade-fotos-animais.component.css'],
+})
+export class GradeFotosAnimaisComponent {
+  @Input() animais!: Animais;
+}
