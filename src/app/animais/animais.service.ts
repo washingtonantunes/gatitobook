b56e2481dcce7e19d5fab446fe96e6191d06dee7
Washington Antunes <washington.cead@gmail.com>
2023-05-01 21:24:58 -0300

    Removendo importes não usados

--- src/app/animais/animais.service.ts
+++ src/app/animais/animais.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Animais, Animal } from './animais';
 import { Observable, catchError, mapTo, of, throwError } from 'rxjs';
