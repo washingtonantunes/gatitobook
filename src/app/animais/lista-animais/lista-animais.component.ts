02bc5c0e3a8d96c339ae1fa8d18b0a84d225d3a2
Washington Antunes <washington.cead@gmail.com>
2023-05-01 21:06:44 -0300

    Guarda Resolver
    Foi criado o resolver com o comando: ng g resolver animais/lista-animais/lista-animais
    Foi alterado o método resolver com a logica de busca para API
    Foi incluído no animais-routing.module.ts o resolver

--- src/app/animais/lista-animais/lista-animais.component.ts
+++ src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Animais } from '../animais';
-import { UsuarioService } from 'src/app/autenticacao/usuario/usuario.service';
-import { AnimaisService } from '../animais.service';
-import { Observable, switchMap } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-lista-animais',
@@ -10,19 +8,11 @@ import { Observable, switchMap } from 'rxjs';
   styleUrls: ['./lista-animais.component.css'],
 })
 export class ListaAnimaisComponent implements OnInit {
-  animais$!: Observable<Animais>;
+  animais!: Animais;
 
-  constructor(
-    private usuarioService: UsuarioService,
-    private animaisService: AnimaisService
-  ) {}
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.animais$ = this.usuarioService.retornaUsuario().pipe(
-      switchMap((usuario) => {
-        const userName = usuario.name ?? '';
-        return this.animaisService.listaDoUsuario(userName);
-      })
-    );
+    this.animais = this.activatedRoute.snapshot.data['animais'];
   }
 }
